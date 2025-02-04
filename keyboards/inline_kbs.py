de1150de26855948b9903cfb921ad733312f3178
IvaMorozilka <ivanmorozko97@gmail.com>
2025-02-04 17:36:32 +0300

    fix: fixed inline keyboard layout it looks nicer this way

--- keyboards/inline_kbs.py
+++ keyboards/inline_kbs.py
@@ -6,5 +6,5 @@ def main_loader_kb():
     builder = InlineKeyboardBuilder()
     for name in dashboard_names:
         builder.button(text=name, callback_data=name)
-    builder.adjust(3, 3, 3, 1)
+    builder.adjust(3, 3, 2, 2)
     return builder.as_markup()
