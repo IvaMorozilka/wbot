a4a5e20da02ec8ef41d8c45ee8a822c7270e6ef0
IvaMorozilka <ivanmorozko97@gmail.com>
2025-02-04 15:50:06 +0300

    feat: new reply kb

--- keyboards/all_kb.py
+++ keyboards/all_kb.py
@@ -2,14 +2,19 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from create_bot import admins
 
 
-def main_kb():
-    kb_list = [[KeyboardButton(text="Данные для ОЭП")]]
+def main_kb(user_id):
+    kb_list = [
+        [KeyboardButton(text="⬇️Загрузить данные для дашборда")],
+        [KeyboardButton(text="🛟Поддержка")],
+    ]
+
+    if user_id in admins:
+        kb_list.append([KeyboardButton(text="⚙️Настройки")])
 
     keyboard = ReplyKeyboardMarkup(
         keyboard=kb_list,
         resize_keyboard=True,
         one_time_keyboard=True,
         input_field_placeholder="Выберите категорию:",
-        
     )
     return keyboard
