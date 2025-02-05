c04bad314791e6370e029374057005051fdc8ed8
IvaMorozilka <ivanmorozko97@gmail.com>
2025-02-05 10:53:06 +0300

    feat: new button cnhange mind

--- keyboards/inline_kbs.py
+++ keyboards/inline_kbs.py
@@ -1,4 +1,5 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.data import dashboard_names
 
 
@@ -6,5 +7,20 @@ def main_loader_kb():
     builder = InlineKeyboardBuilder()
     for name in dashboard_names:
         builder.button(text=name, callback_data=name)
-    builder.adjust(3, 3, 2, 2)
+    builder.button(text="Я передумал", callback_data="change_mind")
+    builder.adjust(3, 3, 2, 2, 1)
     return builder.as_markup()
+
+
+def back_button():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[[InlineKeyboardButton(text="Назад", callback_data="back")]]
+    )
+
+
+def changed_mind_button():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Я передумал", callback_data="back")]
+        ]
+    )
