de27140558e148ac8a295f4e7dd996ac7c8096dd
IvaMorozilka <ivanmorozko97@gmail.com>
2025-03-20 15:30:44 +0300

    refactor: code style change

--- create_bot.py
+++ create_bot.py
@@ -4,22 +4,31 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram.client.session.aiohttp import AiohttpSession
 from asyncpg_lite import DatabaseManager
 
+ADMINS = [int(admin_id) for admin_id in os.environ.get("ADMINS").split(",")]
+DATABASE_URL = os.environ.get("DATABASE_URL")
+DEV_MODE = int(os.environ.get("DEV_MODE"))
+PROXY_URL = os.environ.get("PROXY_URL")
+DEL_PASWD = os.environ.get("DEL_PASWD")
+DEV_TOKEN = os.environ.get("DEV_TOKEN")
+PROD_TOKEN = os.environ.get("PROD_TOKEN")
 
-session = None
-if not config("DEV"):
-    session = AiohttpSession(proxy=config("PROXY_URL"))
+if DEV_MODE:
+    session = None
+    TOKEN = DEV_TOKEN
+else:
+    session = AiohttpSession(proxy=PROXY_URL)
+    TOKEN = PROD_TOKEN
+
 
 scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-ADMINS = [int(admin_id) for admin_id in config("ADMINS").split(",")]
 # DB
 pg_manager = DatabaseManager(
-    db_url=config("DATABASE_URL"),
-    deletion_password=config("DEL_PASWD"),
+    db_url=DATABASE_URL,
+    deletion_password=DEL_PASWD,
     log_level="INFO",
     echo=False,
 )
@@ -32,7 +41,7 @@ logger = logging.getLogger(__name__)
 download_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "downloads")
 
 bot = Bot(
-    token=config("TOKEN"),
+    token=TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     session=session,
 )
